a8a86374ba7cf5b810165a26f71cde7a305b5ca4
Justin <walrathjaw@gmail.com>
2013-02-11 13:25:19 -0500

    Styled the index page.

--- index.php
+++ index.php
@@ -5,28 +5,32 @@
 	<link href="Styles/index.css" rel="stylesheet">
 </head>
 <body>
-	<h1>Welcome to chinese friday unstyled beta.</h1>
-	
-	<a href="CreateOrder.php">
-		<div class='frontPageButtons'>
-			Make An Order
-		</div>
-	</a>
-	<a href="DisplayOrders.php">
-		<div class='frontPageButtons'>
-			View Todays Orders
-		</div>
-	</a>
-	<a href="CreateMeal.php">
-		<div class='frontPageButtons'>
-			Add New Meal Option
-		</div>
-	</a>
-	<a href="ManageUsers.php">
-		<div class='frontPageButtons'>
-			User Manage
-		</div>
-	</a>
+	<div class="container">
+		<h1 id="pageBanner">Welcome to chinese friday unstyled beta.</h1>
+		<a href="CreateOrder.php">
+			<div class='frontPageButtons'>
+				Make An Order
+			</div>
+		</a>
+		<a href="DisplayOrders.php">
+			<div class='frontPageButtons'>
+				View Todays Orders
+			</div>
+		</a>
+		<a href="CreateMeal.php">
+			<div class='frontPageButtons'>
+				Add New Meal Option
+			</div>
+		</a>
+		<a href="ManageUsers.php">
+			<div class='frontPageButtons'>
+				User Manage
+			</div>
+		</a>
+	</div>
 </body>
 </html>
-
+<!--
+Credits:
+	Background image from: 'http://static.freepik.com/free-photo/chinese-pattern-background_18-12593.jpg'
+-->
