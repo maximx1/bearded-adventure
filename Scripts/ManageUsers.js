d69880d7f2db09e2a4475cf7ce98f40c4fab205c
Justin Walrath <walrathjaw@gmail.com>
2013-02-22 22:41:49 -0500

    Corrected further issues with each function call.

--- Scripts/ManageUsers.js
+++ Scripts/ManageUsers.js
@@ -58,10 +58,10 @@ function populateUserTable(userList)
 	//Create the user rows
 	for(i = 0; i < userList.length; i += 1)
 	{
-		$.each(userList[i], function(key, val))
+		$.each(userList[i], function(key, val)
 		{
 			userTable += "<tr><td>"+ val + "</td><td><input class='delete' id='" + key + "' type='button' value='Delete' /></td></tr>";
-		}
+		});
 	}
 	//for(var key in userList)
 	//{
